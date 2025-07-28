08ca9372206c636fd7557440ea30443d23135826
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-07-28 16:11:23 -0300

    test: carry one

--- test/arithmetic/Addition.spec.ts
+++ test/arithmetic/Addition.spec.ts
@@ -75,8 +75,25 @@ class StandardAddition {
     run() {
         const size = Math.max(this._parcela1.getOrder(), this._parcela2.getOrder())
 
+        const carryBuffer = new PlaceValueTable(0)
         for(let i = 0; i < size; i++) {
-            this._total.setPlace(i, (this._parcela1.getPlace(i) || 0) + (this._parcela2.getPlace(i) || 0))
+            const value1 = this._parcela1.getPlace(i) || 0
+            const value2 = this._parcela2.getPlace(i) || 0
+            const carry = carryBuffer.getPlace(i) || 0
+
+            const sum = value1 + value2 + carry
+            this._total.setPlace(i, sum%10)
+            if(sum >= 10) {
+                carryBuffer.setPlace(i+1, 1)
+            }
+
+            console.log(`${i}: ${carry ? `[${carry}] +` : ''} ${value1} + ${value2} = ${this._total.getPlace(i)} ${sum >= 10 ? '(carry one)' : '' }`)
+        }
+
+        if(carryBuffer.getPlace(size)) {
+           this._total.setPlace(size, carryBuffer.getPlace(size))
+            console.log(`${size}: [${carryBuffer.getPlace(size)}] = ${carryBuffer.getPlace(size)}`)
+
         }
     }
 }
@@ -177,7 +194,7 @@ describe('Addition', () => {
 
     test('Should add different order numbers', () => {
         const parcela1 = 32070
-        const parcela2 = 421
+        const parcela2 =   421
 
         const sut = new StandardAddition()
         sut.setParcela1(parcela1)
@@ -186,5 +203,60 @@ describe('Addition', () => {
         sut.run()
 
         expect(sut.getTotal().toNumber()).toBe(32491)
+    })
+
+    test('Should add order 1 numbers with regrouping (carry one)', () => {
+        const parcela1 = 9
+        const parcela2 = 9
+
+        const sut = new StandardAddition()
+        sut.setParcela1(parcela1)
+        sut.setParcela2(parcela2)
+
+        sut.run()
+
+        expect(sut.getTotal().toNumber()).toBe(18)
+        expect(sut.getTotal().getOrder()).toBe(2)
+    })
+
+    test('Should add order 2 numbers with regrouping (carry one)', () => {
+        const parcela1 = 99
+        const parcela2 = 99
+
+        const sut = new StandardAddition()
+        sut.setParcela1(parcela1)
+        sut.setParcela2(parcela2)
+
+        sut.run()
+
+        expect(sut.getTotal().toNumber()).toBe(198)
+        expect(sut.getTotal().getOrder()).toBe(3)
+    })
+
+    test('Should add order 3 numbers with regrouping (carry one)', () => {
+        const parcela1 = 759
+        const parcela2 = 283
+
+        const sut = new StandardAddition()
+        sut.setParcela1(parcela1)
+        sut.setParcela2(parcela2)
+
+        sut.run()
+
+        expect(sut.getTotal().toNumber()).toBe(1042)
+        expect(sut.getTotal().getOrder()).toBe(4)
+    })
+
+    test('Should add different order numbers with regrouping (carry one)', () => {
+        const parcela1 = 32070
+        const parcela2 =   931
+
+        const sut = new StandardAddition()
+        sut.setParcela1(parcela1)
+        sut.setParcela2(parcela2)
+
+        sut.run()
+
+        expect(sut.getTotal().toNumber()).toBe(33001)
     })
 })
