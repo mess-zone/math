825201299ec5a7ab955ef8816679df44ce75938c
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-07-28 18:24:10 -0300

    refactor: addDigits

--- test/arithmetic/Addition.spec.ts
+++ test/arithmetic/Addition.spec.ts
@@ -53,10 +53,12 @@ class StandardAddition {
     private _parcela1: PlaceValueTable
     private _parcela2: PlaceValueTable
     private _total: PlaceValueTable
+    private _carryBuffer: PlaceValueTable
 
     constructor() {
-        this._parcela1 = new PlaceValueTable()
-        this._parcela2 = new PlaceValueTable()
+        this._parcela1 = new PlaceValueTable(0)
+        this._parcela2 = new PlaceValueTable(0)
+        this._carryBuffer = new PlaceValueTable(0)
         this._total = new PlaceValueTable()
     }
     
@@ -72,28 +74,36 @@ class StandardAddition {
         return this._total
     }
 
+    private addDigits(digit1: number, digit2: number, carryDigit: number) {
+        const sum = digit1 + digit2 + carryDigit
+     
+        return {
+            digit: sum % 10,
+            carryFlag: sum >= 10
+        }
+    }
+
     run() {
         const size = Math.max(this._parcela1.getOrder(), this._parcela2.getOrder())
 
-        const carryBuffer = new PlaceValueTable(0)
         for(let i = 0; i < size; i++) {
             const value1 = this._parcela1.getPlace(i) || 0
             const value2 = this._parcela2.getPlace(i) || 0
-            const carry = carryBuffer.getPlace(i) || 0
+            const carry = this._carryBuffer.getPlace(i) || 0
 
-            const sum = value1 + value2 + carry
-            this._total.setPlace(i, sum%10)
-            if(sum >= 10) {
-                carryBuffer.setPlace(i+1, 1)
+            const { digit, carryFlag } = this.addDigits(value1, value2, carry)
+
+            this._total.setPlace(i, digit)
+            if(carryFlag) {
+                this._carryBuffer.setPlace(i + 1, 1)
             }
 
-            console.log(`${i}: ${carry ? `[${carry}] +` : ''} ${value1} + ${value2} = ${this._total.getPlace(i)} ${sum >= 10 ? '(carry one)' : '' }`)
+            console.log(`${i}: ${carry ? `[${carry}] +` : ''} ${value1} + ${value2} = ${this._total.getPlace(i)} ${carryFlag ? '(carry one)' : '' }`)
         }
 
-        if(carryBuffer.getPlace(size)) {
-           this._total.setPlace(size, carryBuffer.getPlace(size))
-            console.log(`${size}: [${carryBuffer.getPlace(size)}] = ${carryBuffer.getPlace(size)}`)
-
+        if(this._carryBuffer.getPlace(size)) {
+           this._total.setPlace(size, this._carryBuffer.getPlace(size))
+            console.log(`${size}: [${this._carryBuffer.getPlace(size)}] = ${this._carryBuffer.getPlace(size)}`)
         }
     }
 }
