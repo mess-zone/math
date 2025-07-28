d44602709d009571d5e00f39b4adbe9d4ba701fd
Gilmar Andrade <gilmar-andrade@outlook.com>
2025-07-28 15:12:27 -0300

    test: adition without overflow

--- test/arithmetic/Addition.spec.ts
+++ test/arithmetic/Addition.spec.ts
@@ -11,12 +11,16 @@ enum Place {
 class PlaceValueTable {
     private table: number[]
 
-    private constructor(array: number[]) {
+    constructor(array: number[]) {
         this.table = array
     }
 
     getPlace(position: number) {
         return this.table[position]
+    }
+
+    getOrder() {
+        return this.table.length
     }
 
     static create(value: number) {
@@ -27,26 +31,30 @@ class PlaceValueTable {
             .map(Number)
             .reverse())
     }
+
+    toNumber() {
+        return +[...this.table].reverse().join('')
+    }
 }
 
 class StandardAddition {
 
-    private _parcela1: number
-    private _parcela2: number
-    private _total: number
+    private _parcela1: PlaceValueTable
+    private _parcela2: PlaceValueTable
+    private _total: PlaceValueTable
 
     constructor() {
-        this._parcela1 = 0
-        this._parcela2 = 0
-        this._total = 0
+        // this._parcela1 = 0
+        // this._parcela2 = 0
+        // this._total = 0
     }
     
     setParcela1(value: number) {
-        this._parcela1 = value
+        this._parcela1 = PlaceValueTable.create(value)
     }
     
     setParcela2(value: number) {
-        this._parcela2 = value
+        this._parcela2 = PlaceValueTable.create(value)
     }
 
     getTotal() {
@@ -54,25 +62,49 @@ class StandardAddition {
     }
 
     run() {
-        this._total = this._parcela1 + this._parcela2
+        const size = Math.max(this._parcela1.getOrder(), this._parcela2.getOrder())
+        console.log(size)
+
+        const result: number[] = []
+        for(let i = 0; i < size; i++) {
+            result.push(this._parcela1.getPlace(i) + this._parcela2.getPlace(i))
+        }
+
+        this._total = new PlaceValueTable(result)
+        
     }
 }
 
 describe('PlaceValueTable', () => {
-    test('Should create a place value table from a number', () => {
+    test('Should create a place value table from a positive number', () => {
+        const placeValue = PlaceValueTable.create(0)
+        expect(placeValue.getPlace(Place.ONES)).toBe(0) 
+        expect(placeValue.getPlace(Place.TENS)).toBe(undefined) 
+        expect(placeValue.getPlace(Place.HUNDREDS)).toBe(undefined) 
+        expect(placeValue.getPlace(Place.THOUSANDS)).toBe(undefined) 
+        expect(placeValue.getPlace(Place.TEN_THOUSANDS)).toBe(undefined) 
+
+        expect(placeValue.getOrder()).toBe(1)
+        expect(placeValue.toNumber()).toBe(0)
+    })
+
+    test('Should create a place value table from a positive number', () => {
         const placeValue = PlaceValueTable.create(345)
         expect(placeValue.getPlace(Place.ONES)).toBe(5) 
         expect(placeValue.getPlace(Place.TENS)).toBe(4) 
         expect(placeValue.getPlace(Place.HUNDREDS)).toBe(3) 
         expect(placeValue.getPlace(Place.THOUSANDS)).toBe(undefined) 
         expect(placeValue.getPlace(Place.TEN_THOUSANDS)).toBe(undefined) 
+
+        expect(placeValue.getOrder()).toBe(3)
+        expect(placeValue.toNumber()).toBe(345)
     })
 
 })
 
 describe('Addition', () => {
 
-    test('Should add', () => {
+    test('Should add order 1 numbers', () => {
         const parcela1 = 2
         const parcela2 = 3
 
@@ -82,6 +114,58 @@ describe('Addition', () => {
 
         sut.run()
 
-        expect(sut.getTotal()).toBe(5)
+        expect(sut.getTotal().toNumber()).toBe(5)
+    })
+
+    test('Should add order 2 numbers', () => {
+        const parcela1 = 32
+        const parcela2 = 43
+
+        const sut = new StandardAddition()
+        sut.setParcela1(parcela1)
+        sut.setParcela2(parcela2)
+
+        sut.run()
+
+        expect(sut.getTotal().toNumber()).toBe(75)
+    })
+
+    test('Should add order 3 numbers', () => {
+        const parcela1 = 320
+        const parcela2 = 431
+
+        const sut = new StandardAddition()
+        sut.setParcela1(parcela1)
+        sut.setParcela2(parcela2)
+
+        sut.run()
+
+        expect(sut.getTotal().toNumber()).toBe(751)
+    })
+
+    test('Should add order 4 numbers', () => {
+        const parcela1 = 3207
+        const parcela2 = 4311
+
+        const sut = new StandardAddition()
+        sut.setParcela1(parcela1)
+        sut.setParcela2(parcela2)
+
+        sut.run()
+
+        expect(sut.getTotal().toNumber()).toBe(7518)
+    })
+
+    test('Should add order 5 numbers', () => {
+        const parcela1 = 32070
+        const parcela2 = 43113
+
+        const sut = new StandardAddition()
+        sut.setParcela1(parcela1)
+        sut.setParcela2(parcela2)
+
+        sut.run()
+
+        expect(sut.getTotal().toNumber()).toBe(75183)
     })
 })
